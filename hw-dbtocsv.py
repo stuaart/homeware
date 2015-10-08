85ad1165e1aad4a050b2948ca294c4c5d47c44e9
Stuart Reeves <stuart@tropic.org.uk>
2015-10-08 16:40:45 +0100

    Fixed CSV

--- hw-dbtocsv.py
+++ hw-dbtocsv.py
@@ -3,28 +3,28 @@
 import sqlite3
 import csv
 
-datadir = "/home/pi/homeware/data"
-dbfile = datadir + "/homeware-data-7-days.db"
+datadir = "/home/pi/homeware/data/"
+dbfile = datadir + "homeware-data-7-days.db"
 
-con = sqlite3.connect(dbfile, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+with sqlite3.connect(dbfile, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES) as con:
 
-with open(datadir + 'env_data_temp1w.csv', 'wb') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-writer.writerow(['timestamp', 'env_data_temp1w'])
-for row in con.execute("select env_data_temp1w from " + table):
-	writer.writerow([str(row[0]), str(row[1])])
+	with open(datadir + 'env_data_temp1w.csv', 'wb') as csvfile:
+		writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		writer.writerow(['timestamp', 'env_data_temp1w'])
+		for row in con.execute("select * from env_data_temp1w"):
+			writer.writerow([str(row[0]), str(row[1])])
 
-with open(datadir + 'env_data_bmp085.csv', 'wb') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-writer.writerow(['timestamp', 'env_data_bmp085_temp', 'env_data_bmp085_pres'])
-for row in con.execute("select env_data_bmp085 from " + table):
-	writer.writerow([str(row[0]), str(row[1]), str(row[2])])
+	with open(datadir + 'env_data_bmp085.csv', 'wb') as csvfile:
+		writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		writer.writerow(['timestamp', 'env_data_bmp085_temp', 'env_data_bmp085_pres'])
+		for row in con.execute("select * from env_data_bmp085"):
+			writer.writerow([str(row[0]), str(row[1]), str(row[2])])
 
-with open(datadir + 'env_data_pir.csv', 'wb') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-writer.writerow(['timestamp', 'env_data_pir_score', 'env_data_pir_period'])
-for row in con.execute("select env_data_pir from " + table):
-	writer.writerow([str(row[0]), str(row[1]), str(row[2])])
+	with open(datadir + 'env_data_pir.csv', 'wb') as csvfile:
+		writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		writer.writerow(['timestamp', 'env_data_pir_score', 'env_data_pir_period'])
+		for row in con.execute("select * from pir_data"):
+			writer.writerow([str(row[0]), str(row[1]), str(row[2])])
 
-con.close()
+#	con.close()
 
--- hw-dbtojson.py
+++ hw-dbtojson.py
@@ -6,26 +6,24 @@ import json
 datadir = "/home/pi/homeware/data"
 dbfile = datadir + "/homeware-data-7-days.db"
 
-con = sqlite3.connect(dbfile, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+with sqlite3.connect(dbfile, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES) as con:
 
-def get(table, posTime, yAxis):
-	o = []
-	n = 1
-	for row in con.execute("select * from " + table):
-		o.append([n, str(row[yAxis]), str(row[posTime])])
-		n += 1
-	return o
+	def get(table, posTime, yAxis):
+		o = []
+		n = 1
+		for row in con.execute("select * from " + table):
+			o.append([n, str(row[yAxis]), str(row[posTime])])
+			n += 1
+		return o
 
-t1 = get("env_data_temp1w", 0, 1)
-t2 = get("env_data_bmp085", 0, 1)
-t3 = get("env_data_bmp085", 0, 2)
-t4 = get("pir_data", 0, 1)
+	t1 = get("env_data_temp1w", 0, 1)
+	t2 = get("env_data_bmp085", 0, 1)
+	t3 = get("env_data_bmp085", 0, 2)
+	t4 = get("pir_data", 0, 1)
 
-env_data = {'env_data' : {'env_data_temp1w' : t1, 'env_data_bmp085_temp' : t2, 'env_data_bmp085_pres' : t3}}
+	env_data = {'env_data' : {'env_data_temp1w' : t1, 'env_data_bmp085_temp' : t2, 'env_data_bmp085_pres' : t3}}
 
-pir_data = {'pir_data' : t4}
-
-con.close()
+	pir_data = {'pir_data' : t4}
 
 
 with open(datadir + '/env_data.json', 'w') as outfile:
