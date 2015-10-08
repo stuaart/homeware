db24689cbbb5f258fe15a4a7f229bdc3dde36470
Stuart Reeves <stuart@tropic.org.uk>
2015-10-08 14:15:08 +0100

    csv updates

--- hw-dbtocsv.py
+++ hw-dbtocsv.py
@@ -9,25 +9,22 @@ dbfile = datadir + "/homeware-data-7-days.db"
 con = sqlite3.connect(dbfile, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
 
 with open(datadir + 'env_data_temp1w.csv', 'wb') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quotechar='|', 
-					    quoting=csv.QUOTE_MINIMAL)
-	writer.writerow(['timestamp', 'env_data_temp1w'])
-	for row in con.execute("select env_data_temp1w from " + table):
-		writer.writerow([str(row[0]), str(row[1])])
+    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+writer.writerow(['timestamp', 'env_data_temp1w'])
+for row in con.execute("select env_data_temp1w from " + table):
+	writer.writerow([str(row[0]), str(row[1])])
 
 with open(datadir + 'env_data_bmp085.csv', 'wb') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quotechar='|', 
-					    quoting=csv.QUOTE_MINIMAL)
-	writer.writerow(['timestamp', 'env_data_bmp085_temp', 'env_data_bmp085_pres'])
-	for row in con.execute("select env_data_bmp085 from " + table):
-		writer.writerow([str(row[0]), str(row[1]), str(row[2]])
+    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+writer.writerow(['timestamp', 'env_data_bmp085_temp', 'env_data_bmp085_pres'])
+for row in con.execute("select env_data_bmp085 from " + table):
+	writer.writerow([str(row[0]), str(row[1]), str(row[2])])
 
 with open(datadir + 'env_data_pir.csv', 'wb') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quotechar='|', 
-					    quoting=csv.QUOTE_MINIMAL)
-	writer.writerow(['timestamp', 'env_data_pir'])
-	for row in con.execute("select env_data_pir from " + table):
-		writer.writerow([str(row[0]), str(row[1])])
+    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+writer.writerow(['timestamp', 'env_data_pir_score', 'env_data_pir_period'])
+for row in con.execute("select env_data_pir from " + table):
+	writer.writerow([str(row[0]), str(row[1]), str(row[2])])
 
 con.close()
 
