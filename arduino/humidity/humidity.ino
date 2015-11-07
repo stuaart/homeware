9509ca3c99638ef17789f3a9b6261af7a84ee1df
Stuart Reeves <stuart@tropic.org.uk>
2015-11-07 18:52:01 +0000

    Updated arduino stuff

--- arduino/humidity/humidity.ino
+++ arduino/humidity/humidity.ino
@@ -1,72 +1,67 @@
-// Example testing sketch for various DHT humidity/temperature sensors
-// Written by ladyada, public domain
 
+// Arduino sketch for humidity/temp sensor and LLAP over radio
+// Adapted from Ciseco example
+
+#include "LLAPSerial.h"
 #include "DHT.h"
 
-#define DHTPIN 2     // what pin we're connected to
+#define CONST 10 // Necessary to scale readings for some reason
 
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+#define DHTPIN 2
+#define DHTTYPE DHT22
 
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+#define BAUD_RATE 115200
 
-// Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
+#define DEVICE_ID "SR"
+#define MSG_STARTED "STARTED"
+#define MSG_ERROR_TEMPERATURE "ERR_TMP"
+#define MSG_ERROR_HUMIDITY "ERR_HUM"
+#define MSG_HUMIDITY "HUM"
+#define MSG_TEMPERATURE "TMP"
+
+#define SAMPLE_FREQ 30000 // 30 seconds
+
 DHT dht(DHTPIN, DHTTYPE);
 
-void setup() {
-  // Serial.begin(9600);
-  pinMode(8, OUTPUT);    // initialize pin 8 to control the radio
-  digitalWrite(8, HIGH); // select the radio
-  Serial.begin(115200);    // start the serial port at 115200 baud (correct for XinoRF and RFu, if using XRF + Arduino you might need 9600)
-  Serial.print("STARTED");// transmit started packet
+void setup() 
+{
+  Serial.begin(BAUD_RATE);
+  pinMode(8, OUTPUT);
+  digitalWrite(8, HIGH);
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);
+  delay(1000); // Radio startup time
+
+  LLAP.init(DEVICE_ID);
   dht.begin();
+  LLAP.sendMessage(F(MSG_STARTED));
 }
 
-void loop() {
-  // Wait a few seconds between measurements.
-  delay(2000);
-
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
+void loop()
+{
+  if (LLAP.bMsgReceived)
+  {
+    Serial.print(F("msg:"));
+    Serial.println(LLAP.sMessage);
+    LLAP.bMsgReceived = false;
   }
 
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
 
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.print(" *C ");
-  Serial.print(f);
-  Serial.print(" *F\t");
-  Serial.print("Heat index: ");
-  Serial.print(hic);
-  Serial.print(" *C ");
-  Serial.print(hif);
-  Serial.println(" *F");
+  static unsigned long lastTime = millis();
+  if (millis() - lastTime >= SAMPLE_FREQ)
+  {
+    int hum = dht.readHumidity() * CONST,
+        temp = dht.readTemperature() * CONST;
+
+    if (isnan(temp))
+      LLAP.sendMessage(F(MSG_ERROR_TEMPERATURE));
+    else
+      LLAP.sendIntWithDP(MSG_TEMPERATURE, temp, 1);
+    if (isnan(hum))
+      LLAP.sendMessage(F(MSG_ERROR_HUMIDITY));
+    else
+      LLAP.sendIntWithDP(MSG_HUMIDITY, hum, 1);
+      
+    lastTime = millis();
+  }
 }
-
