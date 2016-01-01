45b0550d7fc403488459878720115b1f2d775cfc
Stuart Reeves <stuart@tropic.org.uk>
2016-01-01 11:44:44 +0000

    Added VCC measure

--- arduino/humidity/humidity.ino
+++ arduino/humidity/humidity.ino
@@ -8,6 +8,8 @@
 //    Adafruit DHT: https://github.com/adafruit/DHT-sensor-library
 //////////////////////////////////////////////////////////////////////////
 
+// My dev board = 5.06v
+
 #include <LLAPSerial.h>
 #include <DHT.h>
 
@@ -24,7 +26,7 @@
 #define SRF_SLEEP_PIN       4 
 
 
-DHT dht(DHT_PIN, DHT_TYPE); //Humidity: 2-5% accuracy; Temperature: +/- 0.5*c
+DHT dht(DHT_PIN, DHT_TYPE); // Humidity: 2-5% accuracy; Temperature: +/- 0.5*c
 
 void setup() 
 {
@@ -72,6 +74,32 @@ void* readDHT(int *readings)
   }
 }
 
+long readVcc() { // Nicked from http://provideyourown.com/2012/secret-arduino-voltmeter-measure-battery-voltage/
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif  
+
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+
+  long result = (high<<8) | low;
+
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
+}
+
 void loop() 
 {
 /*  // print the string when a newline arrives:
@@ -109,10 +137,12 @@ void loop()
 
     LLAP.sendIntWithDP("HUM", avReadings[0], 1);
     LLAP.sendIntWithDP("TMP", avReadings[1], 1);
-    delay(10);                         // allow radio to finish sending
   }
+
+  LLAP.sendIntWithDP("VCC", readVcc(), 1);
+  delay(10);                            // Allow radio to finish sending
   
-  digitalWrite(SRF_SLEEP_PIN, HIGH); // pull sleep pin high to enter SRF sleep 2
+  digitalWrite(SRF_SLEEP_PIN, HIGH);    // Pull sleep pin high to enter SRF sleep 2
 
   word sc = 0;
   while (sc < FREQ)
